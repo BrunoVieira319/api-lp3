f3fd2252ff4fdafe2374464428764fd72136f209
Bruno da Graça Vieira <brunovieira319@hotmail.com>
2019-05-28 10:04:43 -0300

    [WIP] WeatherService is done

--- src/main/java/ead/fundatec/lp3/myapi/model/Data.java
+++ src/main/java/ead/fundatec/lp3/myapi/model/Data.java
@@ -2,8 +2,6 @@ package ead.fundatec.lp3.myapi.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.time.LocalDate;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Data {
 
@@ -15,8 +13,44 @@ public class Data {
     private Double pressure;
     private String icon;
     private Integer sensation;
+    private String date;
 
     public Data() {
     }
 
+    public Integer getTemperature() {
+        return temperature;
+    }
+
+    public String getWind_direction() {
+        return wind_direction;
+    }
+
+    public Double getWind_velocity() {
+        return wind_velocity;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public Double getPressure() {
+        return pressure;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public Integer getSensation() {
+        return sensation;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
--- src/main/java/ead/fundatec/lp3/myapi/model/Weather.java
+++ src/main/java/ead/fundatec/lp3/myapi/model/Weather.java
@@ -14,7 +14,23 @@ public class Weather {
     public Weather() {
     }
 
+    public String getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Data getData() {
+        return data;
     }
 }
