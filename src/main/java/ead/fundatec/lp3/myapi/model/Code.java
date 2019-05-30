673d5cc364de8e47964249562ce7f025f95fe4dd
Bruno da Graça Vieira <brunovieira319@hotmail.com>
2019-05-30 11:09:45 -0300

    in the response json, secret object excludes keys with null values

--- src/main/java/ead/fundatec/lp3/myapi/model/Code.java
+++ src/main/java/ead/fundatec/lp3/myapi/model/Code.java
@@ -1,8 +1,13 @@
 package ead.fundatec.lp3.myapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Code {
 
     private List<Integer> rgba;
--- src/main/java/ead/fundatec/lp3/myapi/model/Color.java
+++ src/main/java/ead/fundatec/lp3/myapi/model/Color.java
@@ -1,5 +1,10 @@
 package ead.fundatec.lp3.myapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Color {
 
     private String color;
